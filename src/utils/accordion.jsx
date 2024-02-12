7f045c5be2829ac006326673f5536386b9347551
1amrishabh <rb.14101999.bti@gmail.com>
2024-02-12 10:53:39 +0530

    first commit

--- src/utils/accordion.jsx
+++ src/utils/accordion.jsx
@@ -2,19 +2,19 @@ import { HiShieldCheck } from "react-icons/hi";
 import { MdCancel, MdAnalytics } from "react-icons/md";
 const data = [
   {
-    icon: <HiShieldCheck/>,
+    icon: <HiShieldCheck  size={20}/>,
     heading: "Best interest rates on the market",
     detail:
       "Exercitation in fugiat est ut ad ea cupidatat ut in cupidatat occaecat ut occaecat consequat est minim minim esse tempor laborum consequat esse adipisicing eu reprehenderit enim.",
   },
   {
-    icon: <MdCancel />,
+    icon: <MdCancel  size={20}/>,
     heading: "Prevent unstable prices",
     detail:
       "Exercitation in fugiat est ut ad ea cupidatat ut in cupidatat occaecat ut occaecat consequat est minim minim esse tempor laborum consequat esse adipisicing eu reprehenderit enim.",
   },
   {
-    icon: <MdAnalytics />,
+    icon: <MdAnalytics  size={20}/>,
     heading: "Best price on the market",
     detail:
       "Exercitation in fugiat est ut ad ea cupidatat ut in cupidatat occaecat ut occaecat consequat est minim minim esse tempor laborum consequat esse adipisicing eu reprehenderit enim.",
